951c80458c0f72344548f01784bcad43ac6cb85e
madash965 <madash965@gmail.com>
2022-06-30 02:13:28 +0100

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,25 @@
+import 'dart:io';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
 import 'package:wekasuwa/splashScreen.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
 
-Future<void> main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await Permission.camera.request();
+  await Permission.microphone.request();
+  await FlutterDownloader.initialize(
+      debug: false // optional: set false to disable printing logs to console
+      );
+  await Permission.storage.request();
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
+  if (Platform.isAndroid) {
+    await AndroidInAppWebViewController.setWebContentsDebuggingEnabled(true);
+  }
 
   runApp(const App());
 }
