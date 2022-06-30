d1db6bfb92ca5d4da3f05987793d2bc023ecd7d1
madash965 <madash965@gmail.com>
2022-06-30 02:22:45 +0100

    updated

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -6,23 +6,44 @@
 
 #import "GeneratedPluginRegistrant.h"
 
-#if __has_include(<package_info_plus/FLTPackageInfoPlusPlugin.h>)
-#import <package_info_plus/FLTPackageInfoPlusPlugin.h>
+#if __has_include(<flutter_downloader/FlutterDownloaderPlugin.h>)
+#import <flutter_downloader/FlutterDownloaderPlugin.h>
 #else
-@import package_info_plus;
+@import flutter_downloader;
 #endif
 
-#if __has_include(<url_launcher_ios/FLTURLLauncherPlugin.h>)
-#import <url_launcher_ios/FLTURLLauncherPlugin.h>
+#if __has_include(<flutter_inappwebview/InAppWebViewFlutterPlugin.h>)
+#import <flutter_inappwebview/InAppWebViewFlutterPlugin.h>
 #else
-@import url_launcher_ios;
+@import flutter_inappwebview;
+#endif
+
+#if __has_include(<fluttertoast/FluttertoastPlugin.h>)
+#import <fluttertoast/FluttertoastPlugin.h>
+#else
+@import fluttertoast;
+#endif
+
+#if __has_include(<path_provider_ios/FLTPathProviderPlugin.h>)
+#import <path_provider_ios/FLTPathProviderPlugin.h>
+#else
+@import path_provider_ios;
+#endif
+
+#if __has_include(<permission_handler_apple/PermissionHandlerPlugin.h>)
+#import <permission_handler_apple/PermissionHandlerPlugin.h>
+#else
+@import permission_handler_apple;
 #endif
 
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
-  [FLTPackageInfoPlusPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPackageInfoPlusPlugin"]];
-  [FLTURLLauncherPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTURLLauncherPlugin"]];
+  [FlutterDownloaderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterDownloaderPlugin"]];
+  [InAppWebViewFlutterPlugin registerWithRegistrar:[registry registrarForPlugin:@"InAppWebViewFlutterPlugin"]];
+  [FluttertoastPlugin registerWithRegistrar:[registry registrarForPlugin:@"FluttertoastPlugin"]];
+  [FLTPathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPathProviderPlugin"]];
+  [PermissionHandlerPlugin registerWithRegistrar:[registry registrarForPlugin:@"PermissionHandlerPlugin"]];
 }
 
 @end
--- lib/homePage.dart
+++ lib/homePage.dart
@@ -165,3 +165,151 @@ class _HomePage extends State<HomePage> {
         ));
   }
 }
+
+// class HomePage extends StatefulWidget {
+//   const HomePage({Key? key}) : super(key: key);
+
+//   @override
+//   _HomePageState createState() => _HomePageState();
+// }
+
+// class _HomePageState extends State<HomePage> {
+//   late WebViewController controller;
+//   final isDialOpen = ValueNotifier(false);
+//   bool showSpinner = true;
+
+// @override
+// void initState() {
+//   super.initState();
+//   if (Platform.isAndroid) {
+//     WebView.platform = SurfaceAndroidWebView();
+//   } // <<== THIS
+// }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+//       statusBarColor: Colors.white,
+//       statusBarBrightness: Brightness.light,
+//       statusBarIconBrightness: Brightness.dark,
+//       systemNavigationBarColor: Colors.white,
+//       systemNavigationBarIconBrightness: Brightness.dark,
+//     ));
+// return WillPopScope(
+//   onWillPop: () async {
+//     if (await controller.canGoBack()) {
+//       controller.goBack();
+//       // Stay in app
+//       return false;
+//     } else if (isDialOpen.value) {
+//       isDialOpen.value = false;
+//       return false;
+//     } else {
+//       // exit from app
+//       return true;
+//     }
+//   },
+//       child: Scaffold(
+//         body: SafeArea(
+//           child: ModalProgressHUD(
+//             progressIndicator: const CircularProgressIndicator(
+//               color: Colors.orange,
+//             ),
+//             inAsyncCall: showSpinner,
+//             child: WebView(
+//                 initialUrl: 'https://www.wekasuwa.com/',
+//                 javascriptMode: JavascriptMode.unrestricted,
+//                 onPageFinished: (finished) {
+//                   setState(() {
+//                     showSpinner = false;
+//                   });
+//                 },
+//                 onWebViewCreated: (controller) {
+//                   this.controller = controller;
+//                 }),
+//           ),
+//         ),
+//         floatingActionButton: SpeedDial(
+//             animatedIcon: AnimatedIcons.menu_close,
+//             backgroundColor: Colors.orange[800],
+//             overlayOpacity: 0.5,
+//             spacing: 10.0,
+//             spaceBetweenChildren: 8.0,
+//             openCloseDial: isDialOpen,
+//             children: [
+//               SpeedDialChild(
+//                 child: const Icon(Icons.cached),
+//                 label: 'Refresh',
+//                 onTap: () => controller.reload(),
+//               ),
+//               SpeedDialChild(
+//                 child: const Icon(Icons.clear_all_outlined),
+//                 label: 'Cache & Cookies',
+//                 onTap: () {
+//                   controller.clearCache();
+//                   CookieManager().clearCookies();
+//                 },
+//               ),
+//             ]),
+//       ),
+//     );
+//   }
+// }
+
+// child: Scaffold(
+//         body: SafeArea(
+//           child: ModalProgressHUD(
+//             progressIndicator: const CircularProgressIndicator(
+//               color: Colors.orange,
+//             ),
+//             inAsyncCall: spinner,
+//             child: WebView(
+//                 initialUrl: 'https://www.wekasuwa.com/',
+//                 javascriptMode: JavascriptMode.unrestricted,
+//                 onPageFinished: (finished) {
+//                   setState(() {
+//                     spinner = false;
+//                   });
+//                 },
+//                 onWebViewCreated: (controller) {
+//                   this.controller = controller;
+//                 }),
+//           ),
+//         ),
+//         floatingActionButton: FloatingActionButton(
+//           child: const FaIcon(
+//             FontAwesomeIcons.retweet,
+//             size: 24,
+//           ),
+//           backgroundColor: Colors.orange[700],
+//           foregroundColor: Colors.white,
+//           splashColor: Colors.red,
+//           onPressed: () => controller.reload(),
+//         ),
+//       ),
+
+// child: FloatingActionButton(
+//             child: FaIcon(
+//               FontAwesomeIcons.retweet,
+//               size: 24,
+//             ),
+//             backgroundColor: Colors.orange[700],
+//             foregroundColor: Colors.white,
+//             splashColor: Colors.red,
+//             onPressed: () => controller.reload(),
+//           ),
+
+// final difference = DateTime.now().difference(timeBackPressed);
+//           final isExitWarning = difference >= const Duration(seconds: 2);
+
+//           timeBackPressed = DateTime.now();
+
+//           if (isExitWarning) {
+//             const message = 'Press back again to exit!';
+//             Fluttertoast.showToast(msg: message, fontSize: 10);
+
+//             return false;
+//           } else {
+//             Fluttertoast.cancel();
+//             return true;
+//           }
